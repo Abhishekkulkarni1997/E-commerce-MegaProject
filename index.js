d716802a9fde4826d98b226ff561ad55e9be0fd4
Abhishekkulkarni1997 <abhishekkulkarni1997@gmail.com>
2023-02-12 18:40:38 +0530

    connected to db with iife

---
+++ index.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+import app from "./app";
+
+import { config } from "./config/index";
+
+(async () => {
+  try {
+    await mongoose.connect(config.MONGODB_URL);
+    console.log("DB Connected successfully");
+
+    app.on("error", (err) => {
+      console.log("Error ", err);
+      throw err;
+    });
+
+    const onListening = () => {
+      console.log(`listening on ${config.PORT}`);
+    };
+
+    app.listen(config.PORT, onListening);
+  } catch (error) {
+    console.log("ERROR", error);
+    throw error;
+  }
+})();
