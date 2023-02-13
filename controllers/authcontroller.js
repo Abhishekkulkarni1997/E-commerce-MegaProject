7f3b3feef421262fa674639486af381b27a8374e
Abhishekkulkarni1997 <abhishekkulkarni1997@gmail.com>
2023-02-13 10:30:02 +0530

    auth controller login and logout

--- controllers/authcontroller.js
+++ controllers/authcontroller.js
@@ -49,3 +49,60 @@ export const signUp = asyncHandler(async (req, res) => {
     user,
   });
 });
+
+/**************** 
+@login
+@route http://localhost:400/api/auth/login
+@description User login controller
+@params  email , password
+@returns User object
+
+*************************/
+
+export const login = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    throw new customError("Both fields are required", 400);
+  }
+
+  const user = await User.find({ email }).select("+password");
+
+  if (!user) {
+    throw new customError("Incorrect username or password", 400);
+  }
+
+  const isPasswordMatched = await user.comparePassword(password);
+
+  if (isPasswordMatched) {
+    const token = user.getJWTtoken;
+    user.password = undefined;
+    res.cookie("token", token, cookieOptions);
+    return res.status(200).json({
+      success: true,
+      token,
+      user,
+    });
+  }
+  throw new customError("Incorrect username or password", 400);
+});
+
+/**************** 
+@logout
+@route http://localhost:400/api/auth/logout
+@description User logout controller by clearing cookies
+@params  
+@returns User object
+
+*************************/
+
+export const logout = asyncHandler(async (req, res) => {
+  // res.clearCookie()
+  res.cookie("token", null, {
+    expires: new Date(Date.now()),
+    httpOnly: true,
+  });
+  res.status(200).json({
+    success: true,
+    message: "Logged Out",
+  });
+});
