b49273782ef07adb6e57bff233c277edd28c639f
Abhishekkulkarni1997 <abhishekkulkarni1997@gmail.com>
2023-02-13 10:34:05 +0530

    coometns added

--- controllers/authcontroller.js
+++ controllers/authcontroller.js
@@ -97,7 +97,7 @@ export const login = asyncHandler(async (req, res) => {
 
 export const logout = asyncHandler(async (req, res) => {
   // res.clearCookie()
-  // also check nodemailer npm package
+  // also check nodemailer npm package or mailinator
   res.cookie("token", null, {
     expires: new Date(Date.now()),
     httpOnly: true,
