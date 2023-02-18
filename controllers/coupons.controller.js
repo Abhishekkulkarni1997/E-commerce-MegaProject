66ded88b984bdd8657c06e299471058ca590a2ae
Abhishekkulkarni1997 <abhishekkulkarni1997@gmail.com>
2023-02-18 13:50:01 +0530

    coupon controller file created

---
+++ controllers/coupons.controller.js
@@ -0,0 +1,35 @@
+/**************** 
+@createCoupon
+@route http://localhost:4000/api/createCoupon
+@description controller used for creating a coupon & only admin and moderator can create a coupon
+@params 
+@returns coupon object
+
+*************************/
+
+/**************** 
+@deactivateCoupon
+@route http://localhost:4000/api/deactivate/:couponID
+@description controller used for deactivate a coupon & only admin and moderator can deactivate a coupon
+@params 
+@returns coupon object
+
+*************************/
+
+/**************** 
+@deleteCoupon
+@route http://localhost:4000/api/coupon/:couponID
+@description controller used for deleting a coupon & only admin and moderator can delete a coupon
+@params 
+@returns coupon object
+
+*************************/
+
+/**************** 
+@deleteCoupon
+@route http://localhost:4000/api/coupon
+@description controller used for getting a coupon & only admin and moderator can get a coupon
+@params 
+@returns coupon object
+
+*************************/
