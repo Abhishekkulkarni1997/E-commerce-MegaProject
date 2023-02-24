9a51c321025b0920b6eb11d7f37b9947f9522c21
Abhishekkulkarni1997 <abhishekkulkarni1997@gmail.com>
2023-02-24 23:19:41 +0530

    created all coupon controllers

--- controllers/coupons.controller.js
+++ controllers/coupons.controller.js
@@ -1,4 +1,6 @@
-// TODO:
+import Coupon from "../Models/coupon.js";
+import asyncHandler from "../services/asyncHandler.js";
+import customError from "../utils/customError.js";
 
 /**************** 
 @createCoupon
@@ -9,6 +11,32 @@
 
 *************************/
 
+export const createCoupon = asyncHandler(async (req, res) => {
+  const { code, discount, active } = req.body;
+  const { user } = req.user;
+  const extCoupon = await Coupon.findOne({ code });
+
+  if (user.role === "USER") {
+    throw new customError("Not authorized to access this route", 401);
+  }
+
+  if (extCoupon) {
+    throw new customError("This Coupon Code already exists", 400);
+  }
+
+  const coupon = await Coupon.create({
+    code,
+    discount,
+    code,
+  });
+
+  res.status(200).json({
+    success: true,
+    message: "Coupon is created",
+    coupon,
+  });
+});
+
 /**************** 
 @deactivateCoupon
 @route http://localhost:4000/api/deactivate/:couponID
@@ -17,6 +45,28 @@
 @returns coupon object
 
 *************************/
+
+export const deactivateCoupon = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  const { user } = req.user;
+  const extCoupon = await Coupon.findOne({ id });
+
+  if (user.role === "USER") {
+    throw new customError("Not authorized to access this route", 401);
+  }
+
+  if (!extCoupon) {
+    throw new customError("This Coupon Code does not exist", 400);
+  }
+
+  const coupon = await Coupon.findByIdAndUpdate({ id }, { active: false });
+
+  res.status(200).json({
+    success: true,
+    message: "Coupon is deactivated",
+    coupon,
+  });
+});
 
 /**************** 
 @deleteCoupon
@@ -27,11 +77,52 @@
 
 *************************/
 
+export const deleteCoupon = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  const { user } = req.user;
+  const extCoupon = await Coupon.findOne({ id });
+
+  if (user.role === "USER") {
+    throw new customError("Not authorized to access this route", 401);
+  }
+
+  if (!extCoupon) {
+    throw new customError("This Coupon Code does not exist", 400);
+  }
+
+  const coupon = await Coupon.findByIdAndDelete({ id });
+
+  res.status(200).json({
+    success: true,
+    message: "Coupon is deleted",
+    coupon,
+  });
+});
+
 /**************** 
-@deleteCoupon
+@getCoupons
 @route http://localhost:4000/api/coupon
 @description controller used for getting a coupon & only admin and moderator can get a coupon
 @params 
 @returns coupon object
 
 *************************/
+
+export const getCoupons = asyncHandler(async (req, res) => {
+  const { user } = req.user;
+
+  if (user.role === "USER") {
+    throw new customError("Not authorized to access this route", 401);
+  }
+  const coupons = await Coupon.find();
+
+  if (!coupons) {
+    throw new customError("Coupon Codes do not exist", 400);
+  }
+
+  res.status(200).json({
+    success: true,
+    message: "All Coupons",
+    coupons,
+  });
+});
